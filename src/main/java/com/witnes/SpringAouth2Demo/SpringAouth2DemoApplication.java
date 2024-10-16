19003f60fad7290574c22f67be7b0ad6c712dd5b
witnes sostenes <112706208+nshobairwe@users.noreply.github.com>
2024-10-16 20:15:36 +0300

    User login using oauth2(using github)

---
+++ src/main/java/com/witnes/SpringAouth2Demo/SpringAouth2DemoApplication.java
@@ -0,0 +1,13 @@
+package com.witnes.SpringAouth2Demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAouth2DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAouth2DemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/witnes/SpringAouth2Demo/SpringAouth2DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.witnes.SpringAouth2Demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAouth2DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
